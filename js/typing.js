8e00230bb52f3fc2ab0ab44bc9f0f826c537972d
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2022-10-20 10:53:38 +0100

    changing text js

---
+++ js/typing.js
@@ -0,0 +1,31 @@
+const typedTextSpan = document.querySelector("span.typed-text");
+const cursorSpan = document.querySelector(".typed-text-cursor");
+
+const textArray = ["who is looking for internships",
+"who loves art and illustration",
+"who loves to dance",
+"who loves creative, aesthetic web-design",
+"is interested in game design",
+"loves to animate",
+"is always doodling",
+"wants to travel the world"];
+
+const typingDelay= 100;
+const erasingDelay= 60;
+const newTextDelay= 2000;
+// let textArrayIndex = 0;
+let charIndex= 0;
+
+setInterval ("ChangeText()", 1000);
+let textArrayIndex = 0;
+
+
+function ChangeText() {
+    typedTextSpan.textContent = textArray[textArrayIndex];
+        textArrayIndex ++;
+
+    if (textArrayIndex >= textArray.length) {
+        textArrayIndex=0;
+    }
+}
+
