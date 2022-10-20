51ce6f19b9c627803f11e417be1619a7a41e9ed1
Reanna Bakshani <110186374+reannab16@users.noreply.github.com>
2022-10-20 14:31:58 +0100

    typing js

--- js/typing.js
+++ js/typing.js
@@ -1,31 +1,60 @@
 const typedTextSpan = document.querySelector("span.typed-text");
 const cursorSpan = document.querySelector(".typed-text-cursor");
 
-const textArray = ["who is looking for internships",
-"who loves art and illustration",
-"who loves to dance",
-"who loves creative, aesthetic web-design",
-"who is interested in game design",
-"who loves to animate",
-"who is always doodling",
-"who wants to travel the world"];
+const textArray = [
+    "who is looking for internships",
+    "who loves art and illustration",
+    "who loves to dance",
+    "who loves creative, aesthetic web-design",
+    "who is interested in game design",
+    "who loves to animate",
+    "who is always doodling",
+    "who wants to travel the world"];
 
-const typingDelay= 100;
-const erasingDelay= 60;
-const newTextDelay= 2000;
-// let textArrayIndex = 0;
-let charIndex= 0;
-
-setInterval ("ChangeText()", 1000);
+const typingDelay = 100;
+const erasingDelay = 60;
+const newTextDelay = 2000;
+let charIndex = 0;
 let textArrayIndex = 0;
 
-
-function ChangeText() {
-    typedTextSpan.textContent = textArray[textArrayIndex];
-        textArrayIndex ++;
-
-    if (textArrayIndex >= textArray.length) {
-        textArrayIndex=0;
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        setTimeout(erase, newTextDelay);
     }
 }
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) { textArrayIndex = 0; }
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+
+type();
+
+
+// setInterval ("ChangeText()", 1000);
+// let textArrayIndex = 0;
+
+
+// function ChangeText() {
+//     typedTextSpan.textContent = textArray[textArrayIndex];
+//         textArrayIndex ++;
+
+//     if (textArrayIndex >= textArray.length) {
+//         textArrayIndex=0;
+//     }
+// }
 
